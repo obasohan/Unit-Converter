e18b631f930cd0e9640e9484659b8d93f0d23afd
Aisosa Obasohan <aisosaobasohanpre@gmail.com>
2025-01-17 02:35:23 +0000

    Initial Commit

---
+++ Project 2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project 2
+//
+//  Created by Aisosa Obasohan on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project 2Tests/Project_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2Tests.swift
+//  Project 2Tests
+//
+//  Created by Aisosa Obasohan on 17/01/2025.
+//
+
+import Testing
+@testable import Project_2
+
+struct Project_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
